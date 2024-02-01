8579d348af91fa9d27fe98758eae7118ece05d46
yashniks <yashnikalje2@gmail.com>
2024-02-01 16:53:51 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Master from './component/maincomp/Master';
 import Home from './component/maincomp/Home';
 import About from './component/pages/About';
+import { Blog } from './component/pages/Blog';
 
 // import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
@@ -18,6 +19,7 @@ function App() {
     
   <Route path='/' element={<Master Rcf={Home}/>}></Route>
   <Route path='/about' element={<Master Rcf={About}/>}></Route>
+  <Route path='/blog' element={<Master Rcf={Blog}/>}></Route>
   </Routes>
   </BrowserRouter>
     </div>
