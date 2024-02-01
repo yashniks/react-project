0638ea069c7eac5844628c1835e62d0042b70231
Ajspire Technologies <ajspire.dev@gmail.com>
2024-02-01 12:05:46 +0530

    update template & add about page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Master from './component/maincomp/Master';
-import All from './component/maincomp/Home';
-import About from './component/About';
 import Home from './component/maincomp/Home';
+import About from './component/pages/About';
+
 // import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
 
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
   <BrowserRouter>
   <Routes>
+    
   <Route path='/' element={<Master Rcf={Home}/>}></Route>
   <Route path='/about' element={<Master Rcf={About}/>}></Route>
   </Routes>
